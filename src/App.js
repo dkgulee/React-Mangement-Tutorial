93eed1b0d9661cd05b4411e3d3b74c447b25f454
dkgulee <silent10z@naver.com>
2019-08-30 23:02:17 +0900

    Create React Project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-bakcground">
+      <img src={logo} lat="logo"/>
+      <h2>Let's develop mangemeddnt system</h2>
+    </div>
+  );
+}
+
+export default App;
